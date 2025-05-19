0661f4a3b5d7a15f3cedfcdcad6f1ff1ac869b13
Muhammad-Sareer25 <sareer2002@gmail.com>
2025-05-19 06:47:10 +0500

    Fourth commit

---
+++ src/main/java/com/example/todoapplication/Controller.java
@@ -0,0 +1,12 @@
+package com.example.todoapplication;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+    public void addtask(ActionEvent e ){
+
+        System.out.println("NEW TASK");
+    }
+
+}
--- src/main/java/com/example/todoapplication/HelloApplication.java
+++ src/main/java/com/example/todoapplication/HelloApplication.java
@@ -1,6 +1,7 @@
 package com.example.todoapplication;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -24,7 +25,9 @@ public class HelloApplication extends Application {
 
     }
 
+
     public static void main(String[] args) {
+
         launch();
     }
 }
