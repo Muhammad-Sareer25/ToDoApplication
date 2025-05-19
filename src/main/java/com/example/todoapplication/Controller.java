14e7ee4c52e9434f3f2f7fd3daa1a5b87736f752
Muhammad-Sareer25 <sareer2002@gmail.com>
2025-05-19 07:30:00 +0500

    5th commit

--- src/main/java/com/example/todoapplication/Controller.java
+++ src/main/java/com/example/todoapplication/Controller.java
@@ -1,12 +1,32 @@
 package com.example.todoapplication;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 
 public class Controller {
 
-    public void addtask(ActionEvent e ){
 
-        System.out.println("NEW TASK");
+    @FXML     //  inject all the values found in fxml class to controller class.
+     private TextField Txtfield;
+
+    @FXML
+    private VBox taskcontainer;
+
+    public void addtask(ActionEvent e ){
+        // System.out.println("NEW TASK");
+
+
+        String task = Txtfield.getText();
+        if(!task.isEmpty()){
+            Label taskLabel = new Label(task);
+            taskcontainer.getChildren().add(taskLabel);
+
+            taskLabel.getStyleClass().add("taskLabel");
+            Txtfield.clear();
+        }
     }
 
 }
